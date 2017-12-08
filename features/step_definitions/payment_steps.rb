2ae9f7f0e37b72547fc223a5181a1b3341e2d4a1
ScottB <17sb@cis.dk>
2017-12-08 21:52:42 +0100

    Possible fix

--- features/step_definitions/payment_steps.rb
+++ features/step_definitions/payment_steps.rb
@@ -1,10 +1,10 @@
 When("I click {string} button") do |button|
-  click_link_or_button button
-  sleep(2)
-  @stripe_iframe = all('iframe[name=stripe_checkout_app]').last
+  click_button button
 end
 
 When("I fill in stripe form field {string} with {string}") do |field, value|
+  sleep(2)
+  @stripe_iframe = all('iframe[name=stripe_checkout_app]').last
   within_frame @stripe_iframe do
     fill_in field, with: value
   end
@@ -14,5 +14,5 @@ And(/^I submit the stripe form$/) do
   within_frame @stripe_iframe do
     find('.Section-button').click
   end
-  sleep(5)
+  sleep(10)
 end
