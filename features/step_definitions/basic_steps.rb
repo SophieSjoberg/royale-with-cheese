cdfb5d6afe94a6875a425478549e9427e49083dc
Francesco <francesco.falchi@gmail.com>
2018-02-15 15:35:00 +0100

    Edit features and steps with new requirements Small fixes to spec factory and model for product Add shop link to navbar

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -13,3 +13,21 @@ end
 Then("I should see a list of {string}") do |element|
   expect(page).to have_element element
 end
+
+Then("show me the page") do
+  save_and_open_page
+end
+
+Then("I should be redirected to the {string} page") do |page_name|
+  expect(page.current_path).to eq page_path_from(page_name)
+end
+
+def page_path_from(page_name)
+  case page_name.downcase
+    when 'landing' then root_path
+    when 'sign up' then new_user_registration_path
+    when 'login' then new_user_session_path
+    when 'shop' then shop_show_path
+    when 'shopping cart' then shopping_cart_show_path
+  end
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'DB Table' do
+    it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :description }
     it { is_expected.to have_db_column :price }
