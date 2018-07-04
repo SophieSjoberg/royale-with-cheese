f4ad5bb94e827e22fddfb9cb26c9d9414b15bcc2
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-07-04 15:26:08 +0200

    Recreate database before populating

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20180215151556) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.string "price"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
